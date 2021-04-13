36114419d866b8e195814f45b439503b7bb31784
Subham Sharma <33781108+mr-sharmaji@users.noreply.github.com>
2021-04-13 17:51:27 +0530

    make it better

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ print("Success")
 @socketIO.on('time')
 def handle_timestamp(patient_timer_data_object):
     print(patient_timer_data_object)
-    for patientEmail in patient_timer_data_object['patientEmail']:
-        cur.execute("INSERT INTO time_tracker(start_time, end_time, total_seconds, total_active_seconds, patient_email, provider_email, jwt_token, screen_type, cpt_codes) VALUES (to_timestamp(CAST({0} as bigint)/1000), to_timestamp(CAST({1} as bigint)/1000), {2}, {3}, '{4}', '{5}', '{6}', '{7}', '{8}');".format(patient_timer_data_object['start_time'], patient_timer_data_object['end_time'], patient_timer_data_object['total_seconds'], patientEmail, patient_timer_data_object['providerEmail'],patient_timer_data_object['jwt'], patient_timer_data_object['screen_type'], patient_timer_data_object['cpt_codes']))
+    if patient_timer_data_object['type'] == "INSERT_TIME":
+        for patientEmail in patient_timer_data_object['patientEmail']:
+            cur.execute("INSERT INTO time_tracker(start_time, end_time, total_seconds, total_active_seconds, patient_email, provider_email, jwt_token, screen_type, cpt_codes) VALUES (to_timestamp(CAST({0} as bigint)/1000), to_timestamp(CAST({1} as bigint)/1000), {2}, {3}, '{4}', '{5}', '{6}', '{7}', '{8}');".format(patient_timer_data_object['start_time'], patient_timer_data_object['end_time'], patient_timer_data_object['total_seconds'], patient_timer_data_object['total_active_seconds'],patientEmail, patient_timer_data_object['providerEmail'],patient_timer_data_object['jwt'], patient_timer_data_object['screen_type'], patient_timer_data_object['cpt_codes']))
     conn.commit()
     print("Success")
 
